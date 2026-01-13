f1d90aa0ddc72cad2542eec13cfe6a9e7a5c92f3
Deepanshu Rajput <codeindeepc@gmail.com>
2026-01-13 20:03:35 +0530

    Question of function

---
+++ Day 6/qus.js
@@ -0,0 +1,35 @@
+console.log("Code will Run")
+
+// QUESTION 1).  Reverse a number
+
+// let num = 985;
+
+// function reverse_a_number(n)
+// {
+//     // Convert the input number to a string
+//     n = n + "";
+
+//     // Split the string into an array of characters, reverse the array, and then join the characters back into a string
+//     return n.split("").reverse().join("");
+// }
+
+// // Convert the reversed string back to a number and log it to the console
+// console.log(Number(reverse_a_number(num)));
+
+
+// QUESTION 2).Write a JavaScript function that checks whether a passed string is a palindrome or not?
+
+// let str = "MADAM";
+// function palindrome(n){
+//     n = n+""
+//     return n.split("").reverse("").join("");
+// }
+// console.log(String(palindrome(str)));
+
+
+// QUESTION 3).Write a JavaScript function that generates all combinations of a string.
+
+let str = dog;
+function combinations(n){
+    
+}
